6dea3ec6bdd036ce9017b87b578a3c1861c8298e
Pranali <pranalilambat00@gmail.com>
2022-03-08 20:01:29 +0530

    UC-5ReflectionUsingParameterisedConstructor

--- MoodAnalyserApp/MoodAnalyser.cs
+++ MoodAnalyserApp/MoodAnalyser.cs
@@ -6,45 +6,48 @@ namespace MoodAnalyserApp
 {
     public class MoodAnalyser
     {
-            public string message;
-            public MoodAnalyser()
-            { 
+        public string message;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodAnalyser"/> class.
+        /// </summary>
+        public MoodAnalyser() 
+        {
         
-            }
-            /// <summary>
-            /// Initializes a new instance of the <see cref="MoodAnalyser"/> class.
-            /// </summary>
-            /// <param name="message">The message.</param>
-             public MoodAnalyser(string message)
-            {
-                this.message = message;
-            }
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodAnalyser"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
 
-            /// <summary>
-            /// Analyses the mood.
-            /// </summary>
-            /// <returns></returns>
-            /// <exception cref="MoodAnalyserCustomException">
-            /// Mood should not be empty
-            /// or
-            /// Mood should not be null
-            /// </exception>
+        /// <summary>
+        /// Analyses the mood.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="MoodAnalyserCustomException">
+        /// Mood should not be empty
+        /// or
+        /// Mood should not be null
+        /// </exception>
         public string AnalyseMood()
+        {
+            string mood;
+            try
             {
-                string mood;
-                try
+                mood = this.message.Contains("Sad") || this.message.Contains("sad") ? "Sad" : "Happy";
+                if (this.message.Equals(string.Empty))
                 {
-                    mood = this.message.Contains("Sad") || this.message.Contains("sad") ? "Sad" : "Happy";
-                    if (this.message.Equals(string.Empty))
-                    {
-                        throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
-                    }
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
                 }
-                catch (NullReferenceException)
-                {
-                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
-                }
-                return mood;
             }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+            return mood;
         }
     }
+}
--- MoodAnalyserApp/MoodAnalyserFactory.cs
+++ MoodAnalyserApp/MoodAnalyserFactory.cs
@@ -41,6 +41,36 @@ namespace MoodAnalyserApp
                 throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
             }
         }
+
+        /// <summary>
+        /// Creates the object of mood analyser using parameterized constructor.
+        /// </summary>
+        /// <param name="className">Name of the class.</param>
+        /// <param name="constructorName">Name of the constructor.</param>
+        /// <param name="message">The message.</param>
+        /// <returns></returns>
+        /// <exception cref="MoodAnalyserCustomException">
+        /// Constructor not found
+        /// or
+        /// No such class found
+        /// </exception>
+        public static object CreateObjectOfMoodAnalyserUsingParameterizedConstructor(string className, string constructorName, string message = "")
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo info = type.GetConstructor(new[] { typeof(string) });
+                    object instance = info.Invoke(new object[] { (message) });
+                    return instance;
+                }
+                else
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
+            }
+            else
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "No such class found");
+        }
     }
 }
-   
+
